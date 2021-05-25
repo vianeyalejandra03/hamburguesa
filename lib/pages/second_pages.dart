cbf2a71b1e354d231cf17ca4bbdd8e8da7390ecb
FlutLab.io <>
2021-05-25 19:16:22 +0000

    Autosave

--- lib/pages/second_pages.dart
+++ lib/pages/second_pages.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Pagina de ARTICULOS',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
+    final ui.Size logicalSize = MediaQuery.of(context).size;
+    final double _height = logicalSize.height;
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: SafeArea(
+          child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          children: <Widget>{
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+              child: Text(
+                'BURGUER KING',
+                style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 45.0, color: Colors.red),
+              ),
+            ), //fin container
+          }, //fin de widget
+        ), //fin de columna
+      ) //fin de container
+          ), //safearea
+    ); //fin de scaffold
   } // widget
 } // first page
